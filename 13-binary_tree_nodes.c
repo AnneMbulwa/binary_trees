12bc02c3846cf9226d08d45fed96f57873d19ad5
AnneMbulwa <annembulwa602@gmail.com>
2024-01-03 18:40:01 +0300

    binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,8 +11,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left != NULL || tree->right != NULL)
 		sum += binary_tree_nodes(tree->left) + 1
 			+ binary_tree_leaves(tree->right);
+
 	return (sum);
 }
